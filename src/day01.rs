d2ccd0cee74d2525f2a3ad0edc804103684bf690
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2025-12-01 16:27:13 +0100

    Improve Day 01

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,5 @@
+use crate::common::read_input;
+
 enum Direction {
     Left,
     Right,
@@ -8,10 +10,7 @@ struct Instruction {
 }
 
 pub fn day01() {
-    let input_path = "input/day01.txt";
-
-    let instructions: Vec<_> = std::fs::read_to_string(input_path)
-        .unwrap()
+    let (_, part1, part2) = read_input(1)
         .lines()
         .map(|line| {
             let (dir, rest) = line.split_at(1);
@@ -23,29 +22,29 @@ pub fn day01() {
             let distance = rest.parse().unwrap();
             Instruction { dir, distance }
         })
-        .collect();
+        .fold(
+            (50, 0, 0),
+            |(mut current, mut part1, mut part2), Instruction { dir, distance }| {
+                let dir = match dir {
+                    Direction::Left => -1,
+                    Direction::Right => 1,
+                };
 
-    let mut current = 50;
-    let mut part1 = 0;
-    let mut part2 = 0;
-    for Instruction { dir, distance } in instructions {
-        let dir = match dir {
-            Direction::Left => -1,
-            Direction::Right => 1,
-        };
+                for _ in 0..distance {
+                    current += dir;
+                    current %= 100;
+                    if current == 0 {
+                        part2 += 1
+                    }
+                }
 
-        for _ in 0..distance {
-            current += dir;
-            current %= 100;
-            if current == 0 {
-                part2 += 1
-            }
-        }
+                if current == 0 {
+                    part1 += 1;
+                }
 
-        if current == 0 {
-            part1 += 1;
-        }
-    }
+                (current, part1, part2)
+            },
+        );
 
     println!("{part1} {part2}");
 }
