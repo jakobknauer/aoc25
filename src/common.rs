b92c60b4c2ee520d44893e0e52bf37d56c26764c
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2025-12-01 16:26:49 +0100

    Add common.rs

---
+++ src/common.rs
@@ -0,0 +1,19 @@
+pub struct InputFile {
+    contents: String,
+}
+
+impl InputFile {
+    fn new(day: u32) -> Self {
+        let path = format!("input/day{day:02}.txt");
+        let contents = std::fs::read_to_string(path).unwrap();
+        Self { contents }
+    }
+
+    pub fn lines(&self) -> std::str::Lines<'_> {
+        self.contents.lines()
+    }
+}
+
+pub fn read_input(day: u32) -> InputFile {
+    InputFile::new(day)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod common;
 mod day01;
 
 fn main() {
